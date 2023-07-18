cc099f554d77a8eedc35fcee6aea102b812f2bcc
radhakrishan <radhakrishan.r@ril.com>
2023-07-18 19:26:45 +0530

    Initial Commit

---
+++ ScreenTime/ScreenTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenTimeApp.swift
+//  ScreenTime
+//
+//  Created by Mobile Programming  on 18/07/23.
+//
+
+import SwiftUI
+
+@main
+struct ScreenTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
